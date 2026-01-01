fea808aeddf4add85ab7feccab30a89ee79cadd3
dasnai88 <dasnai888@gmail.com>
2026-01-02 00:26:38 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
-use std::io;
 fn main() {
-    let mut num1 = String::new();
-    let mut num2 = String::new();
-    println!("Press num1");
-    io::stdin().read_line(&mut num1).expect("Faile");
-    println!("Press num2");
-    io::stdin().read_line(&mut num2).expect("Faile");
-
-    let data1: i16 = num1.trim().parse().expect("Faile");
-    let data2: u16 = num2.trim().parse().expect("Faile");
-    let res = data1 + data2 as i16;
-    println!("res {}", res);
-    if res == 12 && res > 10 {
-        println!("Hello World!")
-    }
+    let mut v1 = vec![10, 20, 30, 40];
+    v1.push(50);
+    println!("{:?}", v1);
 }
