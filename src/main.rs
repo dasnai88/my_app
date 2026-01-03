397dff820fbe84df777f4a353e2301e9405ad4d7
dasnai88 <dasnai888@gmail.com>
2026-01-03 23:34:12 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,5 @@
-use std::fs;
-use std::io;
+use rand::Rng;
 
 fn main() {
-    let file_name = "out.txt";
-
-    /*match write_to_file(file_name, "Hello Rust") {
-        Ok(()) => println!("File written"),
-        Err(e) => println!("Errors {}", e),
-    }*/
-
-    match file_read(file_name) {
-        Ok(text) => println!("Text: {}", text),
-        Err(e) => println!("Error: {}", e),
-    }
-}
-
-fn file_read(file_patch: &str) -> Result<String, io::Error> {
-    fs::read_to_string(file_patch)
-}
-
-fn write_to_file(file_patch: &str, content: &str) -> Result<(), io::Error> {
-    fs::write(file_patch, content)
+    let mut random = rand;
 }
