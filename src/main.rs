5dedc9cbaa015282d9f692320945c2b7f3f139c6
dasnai88 <dasnai888@gmail.com>
2026-01-05 16:06:26 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,63 @@
 use rand::Rng;
-use std::{i32, io};
+use std::io::{self, Write};
 
-fn read_i32() -> i32 {
+fn read_i32_or_quit() -> Option<i32> {
     loop {
         let mut s = String::new();
         io::stdin().read_line(&mut s).expect("read_line failed");
 
-        match s.trim().parse::<i32>() {
-            Ok(x) => return x,
-            Err(__) => {
-                eprintln!("Введитче целое число (i32)")
-            }
+        let t = s.trim();
+
+        if t.eq_ignore_ascii_case("q") {
+            return None;
+        }
+
+        match t.parse::<i32>() {
+            Ok(x) => return Some(x),
+            Err(_) => eprintln!("Введите целое число (i32) или Q для выхода"),
         }
     }
 }
 
 fn main() {
-    let mut random = rand::rng();
-    let rand: i32 = random.random_range(1..=100);
-    while true {
-        let x = read_i32();
-        if x > rand {
-            println!("you number it`s big ");
-        } else if x < rand {
-            println!("your number it`s small");
-        } else if x == rand {
-            println!("you win");
+    let mut rng = rand::rng();
+
+    loop {
+        let secret: i32 = rng.random_range(1..=100);
+        let mut tries: i32 = 0;
+
+        println!("Я загадал число от 1 до 100.");
+        println!("У вас 7 попыток. Введите число или Q для выхода.");
+
+        loop {
+            if tries >= 7 {
+                println!("Попытки закончились. Загаданное число: {}", secret);
+                break;
+            }
+
+            print!("Попытка {}: ", tries + 1);
+            io::stdout().flush().ok();
+
+            let Some(x) = read_i32_or_quit() else {
+                return; // выходим из программы
+            };
+
+            tries += 1;
+
+            if x > secret {
+                println!("Слишком много");
+            } else if x < secret {
+                println!("Слишком мало");
+            } else {
+                println!("Ты выиграл за {} попыток!", tries);
+                break;
+            }
+        }
+
+        println!("Сыграть ещё раз? (Enter = да, Q = нет)");
+        let mut again = String::new();
+        io::stdin().read_line(&mut again).expect("read_line failed");
+        if again.trim().eq_ignore_ascii_case("q") {
             break;
         }
     }
